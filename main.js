240a34dccd42a5e85902e30f75f943dd5ca33778
Nawaz-yousufzai1 <160153195+Nawaz-yousufzai1@users.noreply.github.com>
2024-03-23 01:13:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// asking question from user through inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter your first Number", type: "number", name: "firstNumber" },
+    { message: "Enter your Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//conditional statement if-else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("invalid input");
+}
